35e24681d022103ee20950815b7f563b0164e2a9
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-03 17:25:29 +0200

    modified

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    // this is a single line comment
+    /*
+    this is multiple line comment
+     */
+    public static void main( String[] args ) {
+
+        // java statement,
+        System.out.println( "Hello World!" );
+        System.out.println("Hello Sweden");
+    }
+}
