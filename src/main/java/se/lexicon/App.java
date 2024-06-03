c4a682b2399c4952aa8dbf01568cd0309a77d4b5
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-03 18:08:05 +0200

    modified

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -14,6 +14,6 @@ public class App {
 
         // java statement,
         System.out.println( "Hello World!" );
-        System.out.println("Hello Sweden");
+        System.out.println("Hello Java");
     }
 }
