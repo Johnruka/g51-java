d6ef98652a01d7aa17ccaa19a6f740822e124d57
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-03 15:03:34 +0200

    Initial commit

---
+++ src/main/java/org/github/g51/App.java
@@ -0,0 +1,20 @@
+package org.github.g51;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    // this is a single line comment
+
+    /* this is a multiple line comment.
+    It can span multiple lines.
+     */
+
+    public static void main( String[] args ) {
+        // java statement,
+        System.out.println( "Hello World!" );
+        System.out.println("Hello java!");
+    }
+}
