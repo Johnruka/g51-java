3c57338e02ef89ca214924a97a0099d7d77af412
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-04 10:20:30 +0200

    modified

--- src/main/java/se/lexicon/VariablesDemo.java
+++
@@ -1,9 +0,0 @@
-package se.lexicon;
-
-public class VariablesDemo {
-
-    public static void main(String [] args) {
-        System.out.println("Hello Java!");
-
-    }
-}
---
+++ src/main/java/se/lexicon/operators.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public class operators {
+
+    public static void main(String[] args) {
+        // operators
+        int number1 = 20;
+        int numer2 = 10;
+        System.out.println(number1);// 20
+        System.out.println(numer2);// 10
+numer2 = number1;
+        System.out.println(number1);// 20;
+        System.out.println(numer2);// 20
+    }
+}
