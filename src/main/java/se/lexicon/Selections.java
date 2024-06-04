dc40f61e8f3882d74d3b55bf88de07ce1b0308bf
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-04 16:37:08 +0200

    modified

---
+++ src/main/java/se/lexicon/Selections.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+public class Selections {
+    public static void main(String[] args) {
+
+        int number = 5;
+        if (number > 0) {
+            System.out.println("this number is positive");}
+    }
+
+
+}
