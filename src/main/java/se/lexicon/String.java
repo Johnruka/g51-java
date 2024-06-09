80539c7ea43fed0a7307db612333e2fe6fca7bc3
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-09 10:27:05 +0200

    modified

---
+++ src/main/java/se/lexicon/String.java
@@ -0,0 +1,23 @@
+package se.lexicon;
+
+public class String {
+    java.lang.String firstName = "John";
+    java.lang.String lastName = "Rukaru";
+    java.lang.String fullName = firstName + "  " +
+
+
+
+    }
+
+}
+
+Public class Main {
+
+    public static void main(String[] args) {
+
+        // String = a reference data type that can store one or more characters.
+
+        String name = "John"
+                name.equals("John")
+    }
+}
