e564d3b66b563989501717207af257a3198f7c73
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-10 20:16:16 +0200

    modified

---
+++ src/main/java/String.java
@@ -0,0 +1,12 @@
+public class String {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void ex1(){
+        // How to create String:
+        String str1 = new String(original:"Hello, World!");
+        String str2 = "Hello, World!";
+    }
+}
