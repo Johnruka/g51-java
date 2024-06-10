98deaf65bfd4dd03a029bff5ae6541d5e3c0cccb
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-10 15:34:19 +0200

    modified

---
+++ src/main/java/se/lexicon/Scopestring.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public class Scopestring {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void length () {
+        // How to create StringString
+        String str1 = new String ("Hello world!");
+        String str2 = "Hello, world";
+
+        String str3 = "This is a concatenated sting.";
+        System.out.println(str3);
+
+})
