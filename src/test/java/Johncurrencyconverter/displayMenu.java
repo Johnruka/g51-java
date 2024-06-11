25d0cfe987a59a6630996e218f765108b6435802
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-11 18:08:26 +0200

    modified

--- src/test/java/Johncurrencyconverter/displayMenu.java
+++ src/test/java/Johncurrencyconverter/displayMenu.java
@@ -1,6 +1,7 @@
 package Johncurrencyconverter;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class displayMenu {
 
@@ -16,3 +17,14 @@ public class displayMenu {
         currencyCodes.put(4, "EUR");
         currencyCodes.put(5, "GBP");
 
+        String fromCode, tocode;
+        double amount;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("This is Johns currency converter");
+
+        System.out.println("Currency converting From?");
+
+        System.out.println("1:Swedish krona \t 2:US Dollar \t 3:Canadian Dollar \t 4:EUR (Euro) \t 5:Great Britain Pound");
+        fromCode = currencyCodes.get(sc.nextInt());
