a939c0c841f6e97e9977152ed461b4b8d458b08b
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-11 17:50:16 +0200

    modified

--- src/test/java/Johncurrencyconverter/displayMenu.java
+++ src/test/java/Johncurrencyconverter/displayMenu.java
@@ -1,4 +1,18 @@
 package Johncurrencyconverter;
 
+import java.util.HashMap;
+
 public class displayMenu {
-}
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> currencyCodes = new HashMap<Integer,String>();
+
+        // Add currency codes
+
+        currencyCodes.put(1, "SEK");
+        currencyCodes.put(2, "USD");
+        currencyCodes.put(3, "CAD");
+        currencyCodes.put(4, "EUR");
+        currencyCodes.put(5, "GBP");
+
