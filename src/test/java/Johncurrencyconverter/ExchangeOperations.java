2180a93b6f9a863a488d5d55e83a39cc8bfb88f2
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-12 12:25:38 +0200

    Created file

---
+++ src/test/java/Johncurrencyconverter/ExchangeOperations.java
@@ -0,0 +1,10 @@
+package Johncurrencyconverter;
+
+public class ExchangeOperations {
+
+    public double convertSekToUsd(double sekAmount){
+        double usdAmount = sekAmount * 0.097851038;
+
+
+    }
+}
