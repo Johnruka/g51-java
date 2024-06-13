f38d2b663424133777100bc48f2f60ed7fa7c267
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-13 19:36:47 +0200

    Modified

--- src/test/java/Johncurrencyconverter/ExchangeRates.java
+++ src/test/java/Johncurrencyconverter/ExchangeRates.java
@@ -4,5 +4,11 @@ public class ExchangeRates {
 
     // static fields
 
-    static double sekToUsd = 0.097851038;
+    static double sekToUsdRates = 0.097851038;
+
+    static double usdToSekRate = 10.2189;
+
+    static double sekToEuroRates = 0.089541932;
+
+    static double euroToSekRates = 11.1680 ;
 }
