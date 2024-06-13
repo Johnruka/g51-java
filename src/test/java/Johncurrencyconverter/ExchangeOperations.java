a08a3d5332facf0b0107148c81822d9b37e1cfa4
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-13 19:30:44 +0200

    created

--- src/test/java/Johncurrencyconverter/ExchangeOperations.java
+++ src/test/java/Johncurrencyconverter/ExchangeOperations.java
@@ -3,6 +3,20 @@ package Johncurrencyconverter;
 public class ExchangeOperations {
 
     public static double convertsekToUsd(double sekAmount) {
-        return sekAmount * 0.097851038;
+        return sekAmount * ExchangeRates.setToUsdRates;
     }
+
+    public static double convertUsdTosek(double usdAmount){
+        return usdAmount * ExchangeRates.usdToSekRate;
+    }
+
+    public static double convertsekToEuro(double sekAmount) {
+        return sekAmount * ExchangeRates.usdToSekRate;
+    }
+
+    public static double convertEuroTosek(double EuroAmount) {
+        return EuroAmount * ExchangeRates.usdToSekRate;
+    }
+
+    // ...
 }
