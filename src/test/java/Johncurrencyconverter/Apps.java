d11009d716f659e66807ddf38d57c206667b7e37
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-14 11:35:45 +0200

    Modified

--- src/test/java/Johncurrencyconverter/Apps.java
+++ src/test/java/Johncurrencyconverter/Apps.java
@@ -3,27 +3,46 @@ package Johncurrencyconverter;
 import java.util.Scanner;
 
 public class Apps {
-    public static void main(String[] args) {
+    public static <displayMenu> void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        displayMenu();
+        public static void displayMenu(displayMenu){
         int choice = scanner.nextInt();
 
-        // performOperations
+        switch (choice){
 
-        // step 1 : display menu
-        // step 2 : get the user choice from the scanner
-        // step 3 : perform operations
+            case 1:
+                // perform sek to Usd
+                break;
+            case 2:
+                // perform Usd to sek
+                break;
+            case 3:
+                // perform sek to Euro
+                break;
+            case 4:
+                // perform Euro to sek
+                break;
+        }
 
+
+
+            // performOperations
+
+            // step 1 : display menu
+            // step 2 : get the user choice from the scanner
+            // step 3 : perform operations
+
+        }
+
+         {
+            System.out.println("Currency Converter App");
+            System.out.println("1. Convert SEK to USD");
+            System.out.println("2. Convert USD to SEK");
+            System.out.println("3. Convert SEK to Euro");
+            System.out.println("4. Convert Euro to SEK");
+            System.out.println("0. EXIT");
+            System.out.println("Enter your choice:");
+        }
     }
 
-    public static void displayMenu(){
-        System.out.println("Currency Converter App");
-        System.out.println("1. Convert SEK to USD");
-        System.out.println("2. Convert USD to SEK");
-        System.out.println("3. Convert SEK to Euro");
-        System.out.println("4. Convert Euro to SEK");
-        System.out.println("0. EXIT");
-        System.out.println("Enter your choice:");
-    }
-}
