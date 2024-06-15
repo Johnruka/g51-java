26f5a4c9050584c572b0a42d6470a0fc4edb5987
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-15 21:27:34 +0200

    Modified

--- src/test/java/Johncurrencyconverter/App1.java
+++ src/test/java/Johncurrencyconverter/App1.java
@@ -1,10 +1,17 @@
 package Johncurrencyconverter;
 
+import java.util.Scanner;
+
 public class App1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         displayMenu();
+        int choice = scanner.nextInt();
+        performOperations(choice); //1
+
+
     }
 
     public static void displayMenu(){
@@ -15,5 +22,28 @@ public class App1 {
         System.out.println("4. convert EURO to SEK");
         System.out.println("0. EXIT");
         System.out.println("Enter your choice: ");
+    }
+
+    public static void performOperations(int operationNumber){
+
+        switch (operationNumber){
+            case 1:
+                System.out.println("operation 1 has been executed!");
+                break;
+            case 2:
+                System.out.println("operation 2 has been executed!");
+                break;
+            case 3:
+                System.out.println("operation 3 has been executed!");
+                break;
+            case 4:
+                System.out.println("operation 4 has been executed!");
+                break;
+            case 0:
+                System.out.println("Exiting the currency App. Goodbye.");
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a valid option.");
+        }
     }
 }
