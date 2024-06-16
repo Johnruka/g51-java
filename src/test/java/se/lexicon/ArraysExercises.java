2adfbcc83be730b5a6bd52734542e79649e9a12d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-16 20:11:55 +0200

    Created

---
+++ src/test/java/se/lexicon/ArraysExercises.java
@@ -0,0 +1,18 @@
+package se.lexicon;
+
+public class ArraysExercises {
+
+    public static <StringArrays> void main(String[] args) {
+        
+        // Array is used to store multiple values in a single variable.
+        
+        int[] numbers = new int[] {20, 40, 50, 60};
+
+        String[] StringArrays = new String[3];
+        
+        
+    }
+        
+        
+    }
+
