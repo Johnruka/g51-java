2f60b8f84db029663027c305aac5e10344ff516c
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-16 20:51:27 +0200

    Created

---
+++ src/test/java/se/lexicon/Letters.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+public class Letters {
+
+    public static void main(String[] args) {
+
+        String[] StringArray = new String[7];
+
+        char[] letters = {'l', 'e', 't', 't', 'e', 'r', 's'};
+        System.out.println(letters.length);
+        System.out.println(letters);
+
+        }
+    }
