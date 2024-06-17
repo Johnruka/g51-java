7ce482215dabb1517c7d809162a861cb657de4e1
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-17 12:40:36 +0200

    Modified

--- src/test/java/se/lexicon/ArraysExercises.java
+++ src/test/java/se/lexicon/ArraysExercises.java
@@ -1,7 +1,6 @@
 package se.lexicon;
 
 
-
 public class ArraysExercises {
 
     public static <StringArrays> void main(String[] args) {
@@ -11,12 +10,36 @@ public class ArraysExercises {
         int[] numbers = new int[] {20, 40, 50, 60};
 
         String[] StringArrays = new String[3];
-        System.out.println(numbers.length);
-        System.out.println("20,40,50,60");
+        System.out.println("20, 40, 50, 60");
         
-        
+
+    }
+
+
+    public static void EX2() {
+
+        String[] cities = new String[4];
+        System.out.println(cities.length);//4
+
+        cities[0] = "Paris";
+        cities[1] = "London";
+        cities[2] = "New York";
+        cities[3] = "stockholm";
+
+        System.out.println(cities[0]);// London
+        System.out.println(cities[1]);// New York
+        System.out.println(cities[2]);// Paris
+        System.out.println(cities[3]);// Stockholm
+
+
+
+    }
+
+
+
+
     }
         
         
-    }
+
 
