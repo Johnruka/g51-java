d26697354ee692bdbfd6cff3b164f14d39bb4d22
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-17 13:40:57 +0200

    Modified

--- src/test/java/se/lexicon/ArraysExercises.java
+++ src/test/java/se/lexicon/ArraysExercises.java
@@ -1,17 +1,20 @@
 package se.lexicon;
 
 
+import java.util.Arrays;
+
 public class ArraysExercises {
 
     public static <StringArrays> void main(String[] args) {
-        
+
         // Array is used to store multiple values in a single variable.
-        
-        int[] numbers = new int[] {20, 40, 50, 60};
+
+        int[] numbers = new int[]{20, 40, 50, 60};
 
         String[] StringArrays = new String[3];
-        System.out.println("20, 40, 50, 60");
-        
+
+
+
 
     }
 
@@ -21,24 +24,20 @@ public class ArraysExercises {
         String[] cities = new String[4];
         System.out.println(cities.length);//4
 
-        cities[0] = "Paris";
-        cities[1] = "London";
-        cities[2] = "New York";
-        cities[3] = "stockholm";
+        String[] Cities = {"Paris", "London", "New York", "Stockholm"};
 
-        System.out.println(cities[0]);// London
-        System.out.println(cities[1]);// New York
-        System.out.println(cities[2]);// Paris
-        System.out.println(cities[3]);// Stockholm
 
+        Arrays.sort(cities);
+        for (String Paris: cities) {
+            System.out.println(Paris);
+        }
+        }
 
 
     }
 
 
 
-
-    }
         
         
 
