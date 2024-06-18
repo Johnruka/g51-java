9568c416b1b66d904019150539a13828c5b8b797
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-18 18:41:42 +0200

    Created

---
+++ src/test/java/se/lexicon/ArraysEX1.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+public class ArraysEX1 {
+
+    public static void main(String[] args) {
+        int[] numbers = {20, 40, 50, 60, 70, 95};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
